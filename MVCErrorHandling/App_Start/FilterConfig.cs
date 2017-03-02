c001fae1f2f47cecb691eaad8d1ee54e1a58487e
Ranjith s <Ranjith s>
2017-03-02 22:43:33 +0530

    First commit

---
+++ MVCErrorHandling/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCErrorHandling
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
